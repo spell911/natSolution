35c1dbccef21f3b5d5ad41f3b1d3d99001f62890
spell911 <spell911@hotmail.co.th>
2017-02-02 13:05:04 +0700

    add about us slide

--- public_html/js_function/main_function.js
+++ public_html/js_function/main_function.js
@@ -1,16 +1,15 @@
-$(document).ready(function() {
+$(document).ready(function () {
     $('#fullpage').fullpage({
         //Navigation
         menu: '#menu',
         lockAnchors: false,
-        anchors:['home', 'aboutus' , 'ourteam' , 'contactus'],
+        anchors: ['home', 'aboutus', 'services', 'portfolio', 'contactus'],
         navigation: true,
         navigationPosition: 'right',
-        navigationTooltips: ['Home', 'About Us' , 'Our Team' , 'Contact Us'],
+        navigationTooltips: ['Home', 'About Us', 'Services', 'Portfolio', 'Contact Us'],
         showActiveTooltip: false,
         slidesNavigation: false,
         slidesNavPosition: 'bottom',
-
         //Scrolling
         css3: true,
         scrollingSpeed: 700,
@@ -38,38 +37,36 @@ $(document).ready(function() {
         touchSensitivity: 15,
         normalScrollElementTouchThreshold: 5,
         bigSectionsDestination: null,
-
         //Accessibility
         keyboardScrolling: true,
         animateAnchor: true,
         recordHistory: true,
-
         //Design
         controlArrows: true,
         verticalCentered: true,
-        sectionsColor : ['#ccc', '#fff'],
+        sectionsColor: ['#ccc', '#fff'],
         paddingTop: '3em',
         paddingBottom: '10px',
         fixedElements: '#header, .footer',
         responsiveWidth: 0,
         responsiveHeight: 0,
         responsiveSlides: true,
-
         //Custom selectors
         sectionSelector: '.section',
         slideSelector: '.slide',
-
         lazyLoading: true,
-
         //events
-        onLeave: function(index, nextIndex, direction){},
-        afterLoad: function(anchorLink, index){},
-        afterRender: function(){},
-        afterResize: function(){},
-        afterResponsive: function(isResponsive){},
-        afterSlideLoad: function(anchorLink, index, slideAnchor, slideIndex){},
-        onSlideLeave: function(anchorLink, index, slideIndex, direction, nextSlideIndex){}
+        onLeave: function (index, nextIndex, direction) {},
+        afterLoad: function (anchorLink, index) {},
+        afterRender: function () {},
+        afterResize: function () {},
+        afterResponsive: function (isResponsive) {},
+        afterSlideLoad: function (anchorLink, index, slideAnchor, slideIndex) {},
+        onSlideLeave: function (anchorLink, index, slideIndex, direction, nextSlideIndex) {}
     });
-    
-    
+});
+
+$('#media').carousel({
+    pause: true,
+    interval: false,
 });
